2d14a816b0d37e23729d4ed624130eec81ddcd7d
Vladimir-Dobr <dobrynin-1986@mail.ru>
2026-04-30 15:38:54 +0300

    Update Обучение1.cpp

--- Обучение1.cpp
+++ Обучение1.cpp
@@ -6,11 +6,11 @@ int main(int argc,char**argv)
 {
 
 int size{};
-int* ptr = new int[size]{};
 
 std::cout<<"enter size array: ";
 std::cin>>size;
-
+int* ptr = new int[size]{};
+  
 for(int i = 0; i < size; i++)
 {	
 std::cout<<"arr"<<"["<<i<<"]"<<" = ";
